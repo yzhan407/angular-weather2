e3883ecd4502c0159c15ce22087ba0b101f6389f
yzhan407 <hehehe2333@hotmail.com>
2022-01-11 15:39:05 -0500

    add chartjs add lodash-es as a dependency for ng2-chartjs finish view for images part add ng2-chartjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyLibModule } from './my-lib/my-lib.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ImagesComponentComponent } from './images-component/images-component.component';
+import { ChartsComponentComponent } from './charts-component/charts-component.component';
+import { NgChartsModule } from './ng2-charts/ng2-chart.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ImagesComponentComponent,
+    ChartsComponentComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     MyLibModule,
+    NgChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
